fe58ade121f1965eafc9ae6029b5f743501f8fa7
Debora Serra <debora.r.serra@gmail.com>
2023-02-27 14:43:50 -0300

    feat: add worker on titles page

--- initial-template/pages/titles/src/services/cardsService.js
+++ initial-template/pages/titles/src/services/cardsService.js
@@ -1,8 +1,10 @@
 export default class CardService {
   #database = []
   #dbUrl = ''
-  constructor({ dbUrl }) {
+  #cardListWorker
+  constructor({ dbUrl, cardListWorker }) {
     this.#dbUrl = dbUrl
+    this.#cardListWorker = cardListWorker
   }
   async loadCards() {
     const response = await fetch(this.#dbUrl)
@@ -14,12 +16,7 @@ export default class CardService {
       .filter(({ title }) => !!keyword ? title.toLowerCase().includes(keyword.toLowerCase()) : true)
 
     if (keyword) {
-      console.log('activating blocking operation...')
-      console.time('blocking-op')
-      // blocking function
-      // 1e5 = 100.000
-      for (let counter = 0; counter < 1e5; counter++) console.log('.')
-      console.timeEnd('blocking-op')
+      this.#cardListWorker.postMessage({ maxItems: 1e5 })
     }
 
     const cards = titles.map(item => {
